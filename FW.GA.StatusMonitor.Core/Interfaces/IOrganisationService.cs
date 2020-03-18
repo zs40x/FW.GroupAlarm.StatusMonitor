573692c9904fce0d31d29c6a90b52de8dac03a04
SMX <stefanmehnert@gmail.com>
2020-03-18 13:01:14 +0100

    Added interface definition to retrieve all organizations.

---
+++ FW.GA.StatusMonitor.Core/Interfaces/IOrganisationService.cs
@@ -0,0 +1,10 @@
+﻿using FW.GA.StatusMonitor.Core.ValueTypes.DTO.GroupAlarm;
+using System.Collections.Generic;
+
+namespace FW.GA.StatusMonitor.Core.Interfaces
+{
+    class IOrganizationService
+    {
+        public List<Organization> All { get; set; }
+    }
+}
