7692a245bc5cbce3b550bd56fcbd7033ec168c7d
SMX <stefanmehnert@gmail.com>
2020-03-18 20:17:30 +0100

    Added label DTO definition.

---
+++ FW.GA.StatusMonitor.Core/ValueTypes/DTO/GroupAlarm/Label.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace FW.GA.StatusMonitor.Core.ValueTypes.DTO.GroupAlarm
+{
+    public class Label
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Color { get; set; }
+        public List<int> Assignees { get; set; }
+    }
+}
