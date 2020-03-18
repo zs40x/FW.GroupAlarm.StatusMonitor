b6b7ce6fac58029f8e004eaeca716f227d8539d9
SMX <stefanmehnert@gmail.com>
2020-03-18 20:52:41 +0100

    Added user dto definition.

---
+++ FW.GA.StatusMonitor.Core/ValueTypes/DTO/GroupAlarm/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FW.GA.StatusMonitor.Core.ValueTypes.DTO.GroupAlarm
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string EMail { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int AvailableStatus { get; set; }
+    }
+}
