03a2c825ce4cca9fe1df1f5b413c27310e266f52
SMX <stefanmehnert@gmail.com>
2020-03-21 13:37:11 +0100

    New model for the totals.

---
+++ FW.GroupAlarm.StatusMonitor/Model/OrganizationTotalsModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FW.GroupAlarm.StatusMonitor.Model
+{
+    public class OrganizationTotalsModel
+    {
+        public int TotalAvailable { get; set; }
+        public int TotalInEvent { get; set; }
+        public int TotalNotAvailable { get; set; }
+        public int TotalRegistrationPending { get; set; }
+    }
+}
