c53d99158146e3466ccad1b659e2b89775c14624
SMX <stefanmehnert@gmail.com>
2020-03-22 14:22:08 +0100

    Implmented authorizationService.

--- Fw.GA.StatusMonitor.Infrastructure/Authorization/UnitAuthorizationService.cs
+++ Fw.GA.StatusMonitor.Infrastructure/Authorization/UnitAuthorizationService.cs
@@ -2,22 +2,30 @@
 using FW.GA.StatusMonitor.Core.ValueTypes.DTO.Authorization;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 
 namespace Fw.GA.StatusMonitor.Infrastructure.Authorization
 {
     public class UnitAuthorizationService : IUnitAuthorizationService
     {
-        private readonly List<AuthorizationMapping> authorizationMappings;
+        private readonly List<AuthorizationMapping> _authorizationMappings;
 
         public UnitAuthorizationService(List<AuthorizationMapping> authorizationMappings)
         {
-            this.authorizationMappings = authorizationMappings ?? throw new ArgumentNullException(nameof(authorizationMappings));
+            _authorizationMappings = authorizationMappings ?? throw new ArgumentNullException(nameof(authorizationMappings));
         }
 
         public bool HasAccess(ClaimsPrincipal user, int unitId)
-        {
-            return false;
-        }
+            => MappingAvailable(unitId) ? IsAuthorized(unitId, user) : true;
+        
+        private bool MappingAvailable(int unitId)
+            => !_authorizationMappings.Any(m => m.UnitId == unitId);
+
+        private bool IsAuthorized(int unitId, ClaimsPrincipal user)
+            =>  user.Claims.Any(c => c.Value.CompareTo(GetAdGroupObjectId(unitId)) == 0);
+
+        private string GetAdGroupObjectId(int unitId)
+            => _authorizationMappings.First(m => m.UnitId == unitId).AdGroupObjectId;
     }
 }
