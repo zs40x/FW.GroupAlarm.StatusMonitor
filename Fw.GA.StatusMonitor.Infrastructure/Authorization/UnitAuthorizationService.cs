d98c780b8b79f2dc9002072134325545408f8c65
SMX <stefanmehnert@gmail.com>
2020-03-22 14:15:51 +0100

    Throw if argument is NULL.

--- Fw.GA.StatusMonitor.Infrastructure/Authorization/UnitAuthorizationService.cs
+++ Fw.GA.StatusMonitor.Infrastructure/Authorization/UnitAuthorizationService.cs
@@ -1,5 +1,6 @@
 ﻿using FW.GA.StatusMonitor.Core.Interfaces;
 using FW.GA.StatusMonitor.Core.ValueTypes.DTO.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Security.Claims;
 
@@ -11,7 +12,7 @@ namespace Fw.GA.StatusMonitor.Infrastructure.Authorization
 
         public UnitAuthorizationService(List<AuthorizationMapping> authorizationMappings)
         {
-            this.authorizationMappings = authorizationMappings;
+            this.authorizationMappings = authorizationMappings ?? throw new ArgumentNullException(nameof(authorizationMappings));
         }
 
         public bool HasAccess(ClaimsPrincipal user, int unitId)
