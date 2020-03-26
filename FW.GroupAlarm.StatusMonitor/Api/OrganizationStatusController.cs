2479c0b72a291f550e00aa950e542e1ae0b92afb
SMX <stefanmehnert@gmail.com>
2020-03-26 17:18:00 +0100

    New initial controller for the organizationStatus API.

---
+++ FW.GroupAlarm.StatusMonitor/Api/OrganizationStatusController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FW.GroupAlarm.StatusMonitor.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrganizationStatusController : ControllerBase
+    {
+    }
+}
