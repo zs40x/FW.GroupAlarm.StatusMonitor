d3fb2afa94d9068dbcfaf4e1bc42b7024a7a0172
SMX <stefanmehnert@gmail.com>
2020-03-26 17:53:31 +0100

    Interface definition for the organization status.

--- FW.GA.StatusMonitor.Core/Interfaces/IOrganizationStatusService.cs
+++ FW.GA.StatusMonitor.Core/Interfaces/IOrganizationStatusService.cs
@@ -1,6 +1,9 @@
-﻿namespace FW.GA.StatusMonitor.Core.Interfaces
+﻿using FW.GA.StatusMonitor.Core.ValueTypes.Model;
+
+namespace FW.GA.StatusMonitor.Core.Interfaces
 {
     interface IOrganizationStatusService
     {
+        OrganizationStatus CurrentStatus();
     }
 }
